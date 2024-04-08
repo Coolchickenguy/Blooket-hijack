87a96a207191c609e12d4d7a60c1744f2393c184
Mr.chicken <chicken.science.lab@outlook.com>
2024-04-08 13:05:15 -0600

    Try firebase again

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const { getFirestore, collection, getDocs } = require("firebase/firestore/lite") 
-
+const yt = {
+    id: "https://id.blooket.com",
+    questionfielder: "https://qf.blooket.com",
+    play: "https://play.blooket.com",
+    dashboard: "https://dashboard.blooket.com",
+    firebase: "https://fb.blooket.com",
+    www: "https://www.blooket.com"
+};
+/*var nA = qa.create({
+    baseURL: yt.firebase,
+    withCredentials: !0
+});*/
 var animalnames /*d*/ = ["Chick", "Chicken", "Cow", "Goat", "Horse", "Pig", "Sheep", "Duck", "Alpaca", "Dog", "Cat", "Rabbit", "Goldfish", "Hamster", "Turtle", "Kitten", "Puppy", "Bear", "Moose", "Fox", "Raccoon", "Squirrel", "Owl", "Hedgehog", "Deer", "Wolf", "Beaver", "Tiger", "Orangutan", "Cockatoo", "Parrot", "Anaconda", "Jaguar", "Macaw", "Toucan", "Panther", "Capuchin", "Gorilla", "Hippo", "Rhino", "Giraffe", "Snowy Owl", "Polar Bear", "Arctic Fox", "Baby Penguin", "Penguin", "Arctic Hare", "Seal", "Walrus"];
 /*_ae*/ var colors = ["Light Blue", "Black", "Red", "Purple", "Pink", "Orange", "Lime", "Green", "Teal", "Tan", "Maroon", "Gray", "Mint", "Salmon", "Burgandy", "Baby Blue", "Dust", "Brown", "Dull Blue", "Yellow", "Blue"];
 var yHe = {
@@ -20,7 +31,7 @@ module.exports = class blooket{
     isRandomNames;
     constructor(){};
     async join(id,name){
-        var landings = await (await fetch("https://classic.blooket.com/api/playersessions/landings",{headers:{
+       /* var landings = await (await fetch("https://classic.blooket.com/api/playersessions/landings",{headers:{
             method: "POST",
             body:JSON.stringify({
 
@@ -28,8 +39,10 @@ module.exports = class blooket{
             headers: {
                 "Content-type": "application/json"
               }
-        }})).json();
-
+        }})).json();*/
+        var app = initializeApp(yHe);
+       console.log(collection(getFirestore(app),"id"));
+return
        var testres = await (await fetch(`https://fb.blooket.com/c/firebase/id?id=${id}`)).json();
         if(testres.success == false){
             throw new Error(testres.msg);
